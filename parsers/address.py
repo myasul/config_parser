791b20ab7a4537970df2543d3803cc732067c80f
Matthew Yasul <yasulmatthew@gmail.com>
2019-04-20 15:38:38 +0800

    Handle multilines for address parser
    Additional changes:
    * add ip type column
    * ip is being populated depending on the ip type

--- parsers/address.py
+++ parsers/address.py
@@ -12,10 +12,12 @@ def generate_address_csv(content, csv_dir, file_format):
     logger = get_logger(__name__)
     logger.info("Generating Address CSV file.")
 
-    addresses = ADDRESS_REGEX.findall(content)
-    addresses_multiline = ADDRESS_MULTILINE_REGEX.findall(content)
-    if addresses_multiline:
-        addresses.append(addresses_multiline)
+    # Address has a special case where it can be within one line
+    # or span between multiple lines. This is handled by two regexes
+    # and stored in a set to avoid duplication.
+    addresses = set()
+    addresses.update(ADDRESS_REGEX.findall(content))
+    addresses.update(ADDRESS_MULTILINE_REGEX.findall(content))
     address_obj = []
 
     parse_count = 0
@@ -34,35 +36,40 @@ def generate_address_csv(content, csv_dir, file_format):
             parsed_config, delimiter=FILE_FORMAT.get(file_format, ','))
         # Write headers
         config_writer.writerow([
-            'ip', 'network_host', 'subnet'])
+            'ip_type', 'ip', 'network_host', 'subnet'])
 
         # Write address entries
         row_count = 0
         for addr in address_obj:
             if addr.host:
                 config_writer.writerow([
+                    addr.ip_type,
                     addr.ip,
                     addr.host,
                     '', ])
             elif addr.network:
                 config_writer.writerow([
+                    addr.ip_type,
                     addr.ip,
                     addr.network,
                     addr.subnet, ])
             else:
                 config_writer.writerow([
+                    addr.ip_type,
                     addr.ip,
                     '',
                     ''])
 
             logger.debug("Adding row {}. Contains {}.".format(
                 row_count, [
+                    addr.ip_type,
                     addr.ip,
                     addr.host,
                     addr.network,
                     addr.subnet
                 ]
             ))
+            row_count += 1
 
         logger.info("Generating Address CSV completed.")
 
@@ -71,6 +78,7 @@ class Address:
     def __init__(self, address, logger):
         self._address = address
         self._logger = logger
+        self.ip_type = ""
         self.ip = ""
         self.host = ""
         self.network = ""
@@ -80,9 +88,7 @@ class Address:
     # Populate fields by extracting the needed data
     # using regular expressions.
     def populate_fields(self):
-        self.ip = helper.extract_field_name(
-            self._address,
-            r'(?<=\s(ipv4|ipv6)\s)')
+        self.ip = self._extract_ip()
         self.host = helper.extract_field_name(
             self._address,
             r'(?<=\shost\s)')
@@ -96,7 +102,41 @@ class Address:
             self.subnet]))
 
     def _extract_ip(self):
-        pass
+        self.ip_type = self._extract_ip_type()
+        if self.ip_type:
+            if self.ip_type == "ipv4":
+                return helper.extract_field_name(
+                    self._address,
+                    pattern=r'(?<=\shost\s)',
+                    flag=re.MULTILINE)
+            elif self.ip_type == "fqdn":
+                return helper.extract_field_name(
+                    self._address,
+                    pattern=r'(?<=\sdomain\s)',
+                    flag=re.MULTILINE)
+            elif self.ip_type == "mac":
+                return helper.extract_field_name(
+                    self._address,
+                    pattern=r'(?<=\saddress\s)',
+                    flag=re.MULTILINE)
+
+            else:
+                return ""
+        else:
+            return ""
+
+    def _extract_ip_type(self):
+        ip_type_match = re.search(
+            r'(?:\sipv4\s)|(?:\sipv6\s)|(?:\sfqdn\s)|(?:\smac\s)',
+            self._address)
+        if ip_type_match:
+            ip_type = ip_type_match.group().strip()
+            if ip_type in ['ipv4', 'ipv6', 'fqdn', 'mac']:
+                return ip_type
+            else:
+                return ""
+        else:
+            return ""
 
     def _extract_network_details(self):
         network_match = re.search(r'(?<=\snetwork\s)' +
--- tools/const.py
+++ tools/const.py
@@ -10,9 +10,9 @@ INTERFACE_FILENAME = 'interface.csv'
 
 SERVICE_REGEX = re.compile(r'^service-object.+$', re.I | re.M)
 SERVICE_GRP_REGEX = re.compile(r'^service-group.+?exit$', re.I | re.M | re.S)
-ADDRESS_REGEX = re.compile(r'^address-object.+$', re.I | re.M)
+ADDRESS_REGEX = re.compile(r'^address-object.+zone.+$', re.I | re.M)
 ADDRESS_MULTILINE_REGEX = re.compile(
-    r'(^address-object.+\n(?:[\s]+.+\n)+\s+exit)', re.I | re.M)
+    r'(^address-object.+\n(?:\s+[\".\s\w\d]+$\n)+?\s+exit)', re.I | re.M)
 ACCESS_RULE_REGEX = re.compile(r'^access-rule.+?(?=exit)', re.I | re.M | re.S)
 ADDRESS_GRP_REGEX = re.compile(r'^address-group.+?exit$', re.I | re.M | re.S)
 ZONE_REGEX = re.compile(r'^zone.+?exit$', re.I | re.M | re.S)
